6d2b0df6429eb3ae6e54e1cdd1f49c08bc7e3d64
Andrew Bayer <andrew.bayer@gmail.com>
2012-01-04 13:30:20 -0800

    Fixing purgeOldBuilds core version.

--- scriptler/purgeOldBuilds.groovy
+++ scriptler/purgeOldBuilds.groovy
@@ -2,7 +2,7 @@
   "name" : "Purge Old Builds",
   "comment" : "Runs the log rotator for each job, purging old builds if needed.",
   "parameters" : [],
-  "core": "1.300",
+  "core": "1.409",
   "authors" : [
     { name : "Andrew Bayer" }
   ]
