86e97006af73d8cc0c601e40ea557ebb6abd7c4b
Andrew Bayer <andrew.bayer@gmail.com>
2012-02-02 15:31:47 -0800

    Clarifying parameters in bulkDeleteOldBuilds.groovy now that Scriptler supports parameterization properly.

--- scriptler/bulkDeleteBuilds.groovy
+++ scriptler/bulkDeleteBuilds.groovy
@@ -9,11 +9,14 @@
 } END META**/
 
 
+// NOTE: uncomment parameters below if not using Scriptler >= 2.0, or if you're just pasting
+// the script in manually.
+
 // The name of the job.
-def jobName = "some-job"
+//def jobName = "some-job"
 
 // The range of build numbers to delete.
-def buildRange = "1-5"
+//def buildRange = "1-5"
 
 import jenkins.model.*;
 import hudson.model.Fingerprint.RangeSet;
