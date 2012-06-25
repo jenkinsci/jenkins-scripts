72c9b71e382fbf23216fc18d82ee528185d691e9
Hayato ITO <haya10.ito@gmail.com>
2012-06-25 23:55:02 +0900

    add checkDiskUsage.groovy

---
+++ scriptler/checkDiskUsage.groovy
@@ -0,0 +1,30 @@
+/*** BEGIN META {
+  "name" : "Check Disk Usage",
+  "comment" : "put disk usage info and check used percentage.",
+  "parameters" : [ threshold ],
+  "core": "1.300",
+  "authors" : [
+    { name : "ITO Hayato" }
+  ]
+} END META**/
+columns = ["path","size","used","free","used%"]
+println columns.join("\t")
+
+result = true;
+
+File.listRoots().each{
+  percent = Math.ceil(it.getTotalSpace() - it.getFreeSpace()) * 100 / it.getTotalSpace()
+  columns = []
+  columns << it.getAbsolutePath()
+  columns << Math.ceil(it.getTotalSpace() /1024/1024)
+  columns << Math.ceil((it.getTotalSpace() - it.getFreeSpace()) /1024/1024)
+  columns << Math.ceil(it.getFreeSpace() /1024/1024)
+  columns << percent + "%"
+
+  println columns.join("\t")
+  
+  if (percent >= Double.valueOf(threshold)) { result = false }
+}
+
+return result
+
