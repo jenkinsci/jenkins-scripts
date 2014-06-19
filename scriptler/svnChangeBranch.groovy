36ecd401c2cca2aba5b6a153b61754929c1270bb
AVee <github@avee.org>
2014-06-20 01:31:52 +0200

    Script to manipulate svn urls
    Based on changeSvnPathVersionNumber,groovy by thosor. This version keeps
    the credentials on the location and uses regexes for matching and
    replacing to be more flexible

---
+++ scriptler/svnChangeBranch.groovy
@@ -0,0 +1,83 @@
+/*** BEGIN META {
+  "name" : "svnChangeBranch",
+  "comment" : "Batch switch svn jobs to a different branch. JobFilter and URL filter are regular expression to select Job/SVN urls to replace. newBranch is the new value to set, oldBranch is the expression to replace (defaults to \'/branches/[^/]* /\' which replaces the url part after branches. No actual changes will be made unless applyChanges is set to \'true\'",
+  "parameters" : [ 'jobFilter', 'urlFilter', 'newBranch', 'oldBranch', 'applyChanges' ],
+  "core": "1.499",
+  "authors" : [
+    { name : "thosor" }, { name : "AVee" }
+  ]
+} END META**/
+
+import hudson.scm.*
+
+if(null == oldBranch || "".equals(oldBranch)) {
+  oldBranch = "/branches/([^/])*/"
+  newBranch = "/branches/$newBranch/"
+}
+
+/** Display Configuration */
+println "### Overwrite SVN-Version ###"
+println "oldVersion:     " + oldBranch
+println "New branch:     " + newBranch
+println "Job Filter:     " + jobFilter
+println "Url Filter:     " + urlFilter
+println "Apply Changes:  " + applyChanges
+
+
+// Access to the Hudson Singleton
+hudsonInstance = hudson.model.Hudson.instance
+
+// Retrieve matching jobs
+allItems = hudsonInstance.items
+chosenJobs = allItems.findAll{job -> job.name =~ /$jobFilter/}
+
+// Do work and create the result table
+chosenJobs.each { job ->
+  if(!(job instanceof hudson.model.ExternalJob)) {
+    // No SCM-Configuration possible for External Jobs!
+    if (job.scm instanceof SubversionSCM) { 
+      println ""
+      println job.name
+      
+      // Job has a SubversionSCM-Configuration
+      def newSvnPath = [][]
+
+      job.scm.locations.each{
+        //For every Subversion-Location
+        println "-   $it.remote"
+
+        if (it.remote =~ /$urlFilter/) {
+          //SVN-Path contains the given Path
+
+          newRemote = it
+
+          match =  it.remote =~ /$oldBranch/
+          
+          if(match) {
+            newRemote = match.replaceFirst(newBranch)
+            println " -> $newRemote"
+            newSvnPath.add(it.withRemote(newRemote))
+          } else {
+            println "    Doesn't match oldBranch"
+            newSvnPath.add(it)
+          }
+        } else {
+          println "    Doesn't match urlFilter"
+        }
+      }
+      
+      // Every Location was checked. Building new SVN-Configuration with the new SVN-Locations
+      newscm = new hudson.scm.SubversionSCM(newSvnPath, job.scm.workspaceUpdater, job.scm.browser, 
+                                   job.scm.excludedRegions, job.scm.excludedUsers, job.scm.excludedRevprop, 
+                                   job.scm.excludedCommitMessages, job.scm.includedRegions,
+                                   job.scm.ignoreDirPropChanges, job.scm.filterChangelog, job.scm.additionalCredentials)
+      if ("true".equals(applyChanges)){
+        // Only write values, when applyChanges is true
+        println "Saving $job.name"
+        job.scm = newscm;
+      }
+      //Job is done
+    }
+  } 
+}
+//done
