de5298676cd57d6944facb32369f65ba4087fcd2
imod <domi@fortysix.ch>
2015-07-06 14:07:28 +0200

    add asserts to check metadata format

--- scriptler/testMetaFormat.groovy
+++ scriptler/testMetaFormat.groovy
@@ -25,10 +25,14 @@ scriptlerDir.eachFileMatch(~/.+\.groovy/) { File f ->
     if (m) {
         try {
             def metadata = JSONObject.fromObject(m[0][1]);
+            assert metadata.name != null : "name version must be set for: ${f.name}"
+            assert metadata.comment != null : "comment must be set for: ${f.name}"            
             metadata['script'] = f.name
             json << metadata
         } catch (Exception e) {
+        	println "metadata for [${f.name}] not correct json" 
             e.printStackTrace(System.err);
+        	throw e 
         }
     }
 }
