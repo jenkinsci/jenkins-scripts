1c3a91b54e4dcc35a3b441fe73e93edb00cf7f06
Sebastian Schuberth <sschuberth@gmail.com>
2015-07-24 09:46:48 +0200

    Add a script to search job configurations

---
+++ scriptler/searchJobConfiguration.groovy
@@ -0,0 +1,32 @@
+/*** BEGIN META {
+  "name" : "Search Job Configuration",
+  "comment" : "Searches job names and configurations for a matching plain text for regexp pattern",
+  "parameters" : ['pattern', 'show'],
+  "core": "1.300",
+  "authors" : [
+    { name : "Sebastian Schuberth" }
+  ]
+} END META**/
+
+def jobs = hudson.model.Hudson.instance.items
+
+if (pattern.startsWith('/') && pattern.endsWith('/')) {
+    println "Searching jobs for regexp ${pattern}..."
+    pattern = pattern.substring(1, pattern.length() - 1)
+    search = 'matches'
+} else {
+    println "Searching jobs for string '${pattern}'..."
+    search = 'contains'
+}
+
+jobs.each { job ->
+    def match = job.configFile.file.find { it."$search"(pattern) } != null
+    if (match || job.name."$search"(pattern)) {
+        println "<a href=\"${job.absoluteUrl}configure\">${job.name}</a> matches"
+        if (show.toBoolean()) {
+            job.configFile.file.findAll { it."$search"(pattern) }.each { println '    ' + it.trim() }
+        }
+    }
+}
+
+null
