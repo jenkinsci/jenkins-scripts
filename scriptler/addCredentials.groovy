7b5f266fb62f642dbac359fec92cd39881f30fc0
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2016-04-20 12:01:27 +0200

    minimal change
    remove java.util package

--- scriptler/addCredentials.groovy
+++ scriptler/addCredentials.groovy
@@ -17,10 +17,10 @@ Credentials c = (Credentials) new UsernamePasswordCredentialsImpl(CredentialsSco
 
 
 def ksm1 = new CertificateCredentialsImpl.FileOnMasterKeyStoreSource(keyfile)
-Credentials ck1 = new CertificateCredentialsImpl(CredentialsScope.GLOBAL,java.util.UUID.randomUUID().toString(), "description", "password", ksm1)
+Credentials ck1 = new CertificateCredentialsImpl(CredentialsScope.GLOBAL,UUID.randomUUID().toString(), "description", "password", ksm1)
 
 def ksm2 = new CertificateCredentialsImpl.UploadedKeyStoreSource(keyfile)
-Credentials ck2 = new CertificateCredentialsImpl(CredentialsScope.GLOBAL,java.util.UUID.randomUUID().toString(), "description", "password", ksm2)
+Credentials ck2 = new CertificateCredentialsImpl(CredentialsScope.GLOBAL,UUID.randomUUID().toString(), "description", "password", ksm2)
 
 SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), c)
 SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), ck1)
