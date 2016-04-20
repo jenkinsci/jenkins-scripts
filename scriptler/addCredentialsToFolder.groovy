80976b54a2f154a18c518bc4080e9891f0851c57
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2016-04-20 11:58:20 +0200

    suggested changes
    Remove unused imports
    Refactor to groovy style loop
    Check for NPE
    
    @Evildethow

--- scriptler/addCredentialsToFolder.groovy
+++ scriptler/addCredentialsToFolder.groovy
@@ -7,24 +7,23 @@
  ]
  } END META**/
 
-import jenkins.model.*
-import com.cloudbees.hudson.plugins.folder.*;
-import com.cloudbees.hudson.plugins.folder.properties.*;
-import com.cloudbees.hudson.plugins.folder.properties.FolderCredentialsProvider.FolderCredentialsProperty;
-import com.cloudbees.plugins.credentials.impl.*;
-import com.cloudbees.plugins.credentials.*;
-import com.cloudbees.plugins.credentials.domains.*;
+import com.cloudbees.hudson.plugins.folder.properties.FolderCredentialsProvider.FolderCredentialsProperty
+import com.cloudbees.hudson.plugins.folder.AbstractFolder
+import com.cloudbees.hudson.plugins.folder.Folder
+import com.cloudbees.plugins.credentials.impl.*
+import com.cloudbees.plugins.credentials.*
+import com.cloudbees.plugins.credentials.domains.*
 
-jenkins = Jenkins.instance
-
-String id = java.util.UUID.randomUUID().toString()
+String id = UUID.randomUUID().toString()
 Credentials c = new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL, id, "description:"+id, "user", "password")
 
-for (folder in jenkins.getAllItems(Folder.class)) {
-  if(folder.name.equals('FolderName')){
-	AbstractFolder<?> folderAbs = AbstractFolder.class.cast(folder)
-    FolderCredentialsProperty property = folderAbs.getProperties().get(FolderCredentialsProperty.class)
-    property.getStore().addCredentials(Domain.global(), c)
-    println property.getCredentials().toString()
-  }
-}
+Jenkins.instance.getAllItems(Folder.class)
+    .findAll{it.name.equals('FolderName')}
+    .each{
+        AbstractFolder<?> folderAbs = AbstractFolder.class.cast(it)
+        FolderCredentialsProperty property = folderAbs.getProperties().get(FolderCredentialsProperty.class)
+        if(property != null){
+            property.getStore().addCredentials(Domain.global(), c)
+            println property.getCredentials().toString()
+        }
+    }
